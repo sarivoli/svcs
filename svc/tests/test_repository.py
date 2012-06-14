4f9b1c77bf30ba92373b2c9ad56a76c4b04d651c
Arivoli <sarivoli@zeomega.com>
2012-06-14 15:25:32 +0530

    added assertion statement in the test case and test cases is failing now

--- svc/tests/test_repository.py
+++ svc/tests/test_repository.py
@@ -1,9 +1,17 @@
 import py
 import os
+import shutil
 
 from ..repository import Repo
 
 def test_initrepo():
     """Testing Inilialization of repository"""
     curDir = "/tmp/test"
+    #Check if the directory exists or not
+    if os.path.exists(curDir):
+        shutil.rmtree(curDir)
     curRepo = Repo(curDir)
+    
+    #After initilizing the repo need to check repo structure
+    assert os.path.exists(os.path.join(curDir , '.svc','objects'))
+    
