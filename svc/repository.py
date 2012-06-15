d2ef6b145dbe6cc2fd32c483f00a3b39c7f5aa09
Arivoli <sarivoli@zeomega.com>
2012-06-15 12:04:21 +0530

    added test case for logs test case fails as of now because , log method is not yet implemented

--- svc/repository.py
+++ svc/repository.py
@@ -1,19 +1,46 @@
 import os
-class InvalidRepo(Exception): 
-    def __init__(self, errMsg):
-        Exception.__init__(self,errMsg)
-        self.message = errMsg
-
+import datetime
+from  storage import FileStorage,File,Commit
+class InvalidRepo(Exception): pass        
+class InvalidFile(IOError): pass
+        
 class Repo:
-    def __init__(self,wd):
-        if not os.path.exists(wd):
+    def __init__(self, repoDir):
+        if not os.path.exists(repoDir):
             raise InvalidRepo("Invalid Repo path")
         #creating required structure
-        self.path = wd
+        self.path = repoDir
+        self.storage  = FileStorage(os.path.join(repoDir,".svcs"))
         #create objects directory under.svcs directory
-        os.makedirs(os.path.join(wd,".svcs","objects"))
-        os.makedirs(os.path.join(wd,".svcs","tip"))
+        #os.makedirs(os.path.join(wd,".svcs","objects"))
+        #os.makedirs(os.path.join(wd,".svcs","tip"))
+        
+        
     def commit(self,commitMsg, userId, listOfFiles):
         """stre all the give files"""
+        latestCommitId =None
+        date = datetime.datetime.utcnow().replace(microsecond = 0)
+        #parent = None
+        files =[]
+        for itm in listOfFiles:
+            filePath = os.path.join(self.path,itm)
+            if not os.path.exists(filePath):
+                raise InvalidFile
+            
+            fd= open(filePath, "r")
+            fileObj =File(fd.read())
+            self.storage.store_object(fileObj)
+            fd.close()
+            latestCommitId = fileObj.id
+            files.append([itm,fileObj.id])
+            
+        parent = self.storage.tip
+        if parent.find('TIP')>-1:
+            parent=None
+        comObj = Commit(userId,commitMsg, date,parent,files)
+        self.storage.store_object(comObj)
+        self.storage.update_tip(comObj)
+
+        return latestCommitId
+            
         
-        pass
--- svc/tests/test_repository.py
+++ svc/tests/test_repository.py
@@ -16,7 +16,7 @@ def test_initrepo():
     
     #After initilizing the repo need to check repo structure
     assert os.path.exists(os.path.join(curDir , '.svcs','objects'))
-    assert os.path.exists(os.path.join(curDir , '.svcs','tip'))
+    assert os.path.exists(os.path.join(curDir , '.svcs','TIP'))
     
 def test_commit():
     """ Testing commit process"""
@@ -31,6 +31,34 @@ def test_commit():
     fd= open(os.path.join(curDir,fileName), "w")
     fd.write("This is a new content to commit")
     fd.close()
-    curRepo.commit("Committing a new file",userId,[fileName])
-    assert os.path.exists(os.path.join(curDir,'.svcs','objects',fileName))
+    latestCommitId = curRepo.commit("Committing a new file",userId,[fileName])
+    assert os.path.exists(os.path.join(curDir,'.svcs','objects',latestCommitId))
+
+def test_logs():
+    """ Testing to get logs for a given repo"""
+    curDir = "/tmp/test"
+    #Check if the directory exists or not
+    if not os.path.exists(curDir):
+        os.mkdir(curDir)
+    curRepo = Repo(curDir)
+    
+    #commit a new file
+    fileName = "myfile1.txt"
+    userId = "sarivoli@zeomega.com"
+    commitMsg = "New File to commit"
+
+    fd= open(os.path.join(curDir,fileName), "w")
+    fd.write("This is a new content to commit to check log")
+    fd.close()
+    
+    latestCommitId = curRepo.commit(commitMsg,userId,[fileName])
+
+    logs = curRepo.getLogs()
+    found = False
+    for itm in logs:
+        print itm.id + "   " + itm.message
+        if itm.id ==latestCommitId:
+            found=True
+    assert found
+        
     
